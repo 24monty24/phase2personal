b88b219f45d9d8d6bc28e395fa043b1fe3fd9059
M White <monty__24@yahoo.com>
2017-11-17 01:41:24 +0000

    upload to yellowjackettravel

--- script.js
+++ script.js
@@ -10,12 +10,21 @@
 // })
 
 
-$(window).one('resize', function(event){
-  var windowWidth = $(window).width();
-  console.log(windowWidth);
-  if(windowWidth < 750) {
-    $('.separator').append('<img src="images/line-2.png">');
-  } else {
-    $('.separator').empty()
-  }
-})
+// $(window).one('resize', function(event){
+//   var windowWidth = $(window).width();
+//   console.log(windowWidth);
+//   if(windowWidth < 750) {
+//     $('.separator').append('<img src="images/line-2.png">');
+//   } else {
+//     $('.separator').empty()
+//   }
+// })
+
+
+window.onbeforeunload = function() {
+    return "You're leaving the site.";
+};
+$(document).ready(function() {
+    $('a[rel!=ext]').click(function() { window.onbeforeunload = null; });
+    $('form').submit(function() { window.onbeforeunload = null; });
+});
